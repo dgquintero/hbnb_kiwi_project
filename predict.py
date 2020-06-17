d56b6deea3fa524adfca4c09490cd921b2d01a06
Alejo Ortiz <davidalejandro64@gmail.com>
2020-06-17 02:35:12 -0500

    add data and modified files broken

--- predict.py
+++ predict.py
@@ -1,3 +1,32 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0f2fbd0966c081b3928482bbdf8ecb9b12d5cd20bcf5d8c75fb503d55b229b6c
-size 783
+import numpy as np
+import tensorflow as tf
+import keras
+from keras.initializers import glorot_uniform
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+
+longitud, altura = 100, 100
+modelo = './modelo/modelo.h5'
+pesos_modelo = './modelo/pesos.h5'
+cnn = tf.keras.models.load_model(modelo)
+cnn.load_weights(pesos_modelo)
+
+def predict(file):
+  x = load_img(file, target_size=(longitud, altura))
+  x = img_to_array(x)
+  x = np.expand_dims(x, axis=0)
+  array = cnn.predict(x)
+  result = array[0]
+  answer = np.argmax(result)
+  if answer == 0:
+    print("pred: Near")
+  elif answer == 1:
+    print("pred: Far")
+  elif answer == 2:
+    print("pred: Medium")
+  elif answer == 3:
+    print("pred: Super Far")
+
+  return answer
+
+predict('prueba8.jpg')
